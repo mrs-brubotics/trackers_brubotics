9b8ec58ad85b5c4d99fc5b4162444772c687616a
Bryan Convens <bryan.convens@gmail.com>
2020-12-04 10:00:55 +0100

    added DergbryanTracker as a copy of BypassTracker

---
+++ src/dergbryan_tracker/dergbryan_tracker.cpp
@@ -0,0 +1,329 @@
+#define VERSION "0.0.0.0"
+
+/*includes//{*/
+#include <ros/ros.h>
+#include <mrs_uav_managers/tracker.h>
+#include <mrs_lib/profiler.h>
+#include <mrs_lib/mutex.h>
+
+//}
+
+namespace mrs_uav_trackers
+{
+
+namespace dergbryan_tracker
+{
+
+/*class DergbryanTracker//{*/
+class DergbryanTracker : public mrs_uav_managers::Tracker {
+public:
+  void                          initialize(const ros::NodeHandle &parent_nh, const std::string uav_name, std::shared_ptr<mrs_uav_managers::CommonHandlers_t> common_handlers);
+  std::tuple<bool, std::string> activate(const mrs_msgs::PositionCommand::ConstPtr &last_position_cmd);
+  void                          deactivate(void);
+  bool                          resetStatic(void);
+
+  const mrs_msgs::PositionCommand::ConstPtr update(const mrs_msgs::UavState::ConstPtr &uav_state, const mrs_msgs::AttitudeCommand::ConstPtr &last_attitude_cmd);
+  const mrs_msgs::TrackerStatus             getStatus();
+  const std_srvs::SetBoolResponse::ConstPtr enableCallbacks(const std_srvs::SetBoolRequest::ConstPtr &cmd);
+  const std_srvs::TriggerResponse::ConstPtr switchOdometrySource(const mrs_msgs::UavState::ConstPtr &new_uav_state);
+
+  const mrs_msgs::ReferenceSrvResponse::ConstPtr           setReference(const mrs_msgs::ReferenceSrvRequest::ConstPtr &cmd);
+  const mrs_msgs::TrajectoryReferenceSrvResponse::ConstPtr setTrajectoryReference(const mrs_msgs::TrajectoryReferenceSrvRequest::ConstPtr &cmd);
+
+  const mrs_msgs::DynamicsConstraintsSrvResponse::ConstPtr setConstraints(const mrs_msgs::DynamicsConstraintsSrvRequest::ConstPtr &cmd);
+
+  const std_srvs::TriggerResponse::ConstPtr hover(const std_srvs::TriggerRequest::ConstPtr &cmd);
+  const std_srvs::TriggerResponse::ConstPtr startTrajectoryTracking(const std_srvs::TriggerRequest::ConstPtr &cmd);
+  const std_srvs::TriggerResponse::ConstPtr stopTrajectoryTracking(const std_srvs::TriggerRequest::ConstPtr &cmd);
+  const std_srvs::TriggerResponse::ConstPtr resumeTrajectoryTracking(const std_srvs::TriggerRequest::ConstPtr &cmd);
+  const std_srvs::TriggerResponse::ConstPtr gotoTrajectoryStart(const std_srvs::TriggerRequest::ConstPtr &cmd);
+
+private:
+  // // | ------------------------ uav state ----------------------- |
+  mrs_msgs::UavState uav_state_;
+  bool               got_uav_state_ = false; // now added by bryan
+  std::mutex         mutex_uav_state_; // now added by bryan
+
+  double uav_x_; // now added by bryan
+  double uav_y_; // now added by bryan
+  double uav_z_; // now added by bryan
+
+
+
+
+
+  // // | ------------------- the state variables ------------------ |
+  std::mutex mutex_state_;
+
+
+
+
+  // | ---------------------- desired goal ---------------------- |
+  double     goal_x_;
+  double     goal_y_;
+  double     goal_z_;
+  double     goal_heading_;
+  double     have_goal_ = false;
+  std::mutex mutex_goal_;
+
+
+  mrs_lib::Profiler profiler_;
+
+  bool is_initialized_ = false;
+  bool is_active_      = false;
+  bool hover_          = false;
+
+  bool starting_bool=true;
+};
+//}
+
+//WRITE THE FUNCTIONS HERE.
+
+
+/*initialize()//{*/
+void DergbryanTracker::initialize(const ros::NodeHandle &parent_nh, [[maybe_unused]] const std::string uav_name,
+                             [[maybe_unused]] std::shared_ptr<mrs_uav_managers::CommonHandlers_t> common_handlers) {
+  ros::Time::waitForValid();
+  // no paramaters to be loaded for this tracker
+  ROS_INFO("[DergbryanTracker]: no parameters to be loaded");
+  // initialize variables
+
+  // initialization completed
+  is_initialized_ = true;
+  ROS_INFO("[DergbryanTracker]: initialized");
+}
+//}
+/*activate()//{*/
+std::tuple<bool, std::string> DergbryanTracker::activate(const mrs_msgs::PositionCommand::ConstPtr &last_position_cmd) {
+  std::stringstream ss;
+  if (!got_uav_state_) {
+    ss << "odometry not set";
+    ROS_ERROR_STREAM("[DergbryanTracker]: " << ss.str());
+    return std::tuple(false, ss.str());
+  }
+
+  ss << "Activated";
+  is_active_ = true;
+  ROS_INFO("[DergbryanTracker]: activated");
+  return std::tuple(true, ss.str());
+}
+
+/*deactivate()//{*/
+void DergbryanTracker::deactivate(void) {
+  is_active_ = false;
+  ROS_INFO("[DergbryanTracker]: deactivated");
+}
+//}
+
+/*resetStatic()//{*/
+bool DergbryanTracker::resetStatic(void) {
+  ROS_INFO("[DergbryanTracker]: no states to reset");
+  return true;
+}
+//}
+
+/*update()//{*/
+const mrs_msgs::PositionCommand::ConstPtr DergbryanTracker::update(const mrs_msgs::UavState::ConstPtr &                        uav_state,
+                                                              [[maybe_unused]] const mrs_msgs::AttitudeCommand::ConstPtr &last_attitude_cmd) {
+
+  mrs_lib::Routine profiler_routine = profiler_.createRoutine("update");
+
+  {
+    std::scoped_lock lock(mutex_uav_state_);
+
+    uav_state_ = *uav_state;
+    uav_x_     = uav_state_.pose.position.x;
+    uav_y_     = uav_state_.pose.position.y;
+    uav_z_     = uav_state_.pose.position.z;
+
+    got_uav_state_ = true;
+  }
+
+  // up to this part the update() method is evaluated even when the tracker is not active
+  if (!is_active_) {
+    return mrs_msgs::PositionCommand::Ptr();
+  }
+  mrs_msgs::PositionCommand position_cmd;
+
+  // set the header
+  position_cmd.header.stamp    = uav_state->header.stamp;
+  position_cmd.header.frame_id = uav_state->header.frame_id;
+  if (starting_bool) {
+
+    goal_x_= uav_state->pose.position.x;
+    goal_y_= uav_state->pose.position.y;
+    goal_z_= uav_state->pose.position.z;
+    // set heading based on current odom
+    try {
+      goal_heading_ = mrs_lib::AttitudeConverter(uav_state->pose.orientation).getHeading();
+      position_cmd.use_heading = 1;
+    }
+    catch (...) {
+      position_cmd.use_heading = 0;
+      ROS_ERROR_THROTTLE(1.0, "[DergbryanTracker]: could not calculate the current UAV heading");
+    }
+
+    position_cmd.position.x     = goal_x_;
+    position_cmd.position.y     = goal_y_;
+    position_cmd.position.z     = goal_z_;
+    position_cmd.heading        = goal_heading_;
+
+    position_cmd.use_position_vertical   = 1;
+    position_cmd.use_position_horizontal = 1;
+    position_cmd.use_velocity_vertical   = 1;
+    position_cmd.use_velocity_horizontal = 1;
+    position_cmd.use_acceleration        = 0;
+    position_cmd.use_jerk                = 0;
+    position_cmd.use_heading             = 1;
+    position_cmd.use_heading_rate        = 1;
+
+    starting_bool=false;
+
+    ROS_INFO("[Dergbryan tracker - odom]: [goal_x_=%.2f],[goal_y_=%.2f],[goal_z_=%.2f, goal_heading_=%.2f]",goal_x_,goal_y_,goal_z_,goal_heading_);
+ 
+  return mrs_msgs::PositionCommand::ConstPtr(new mrs_msgs::PositionCommand(position_cmd));
+}
+
+ // set the desired states from the input of the goto function
+
+  position_cmd.position.x     = goal_x_;
+  position_cmd.position.y     = goal_y_;
+  position_cmd.position.z     = goal_z_;
+  position_cmd.heading        = goal_heading_;
+
+  position_cmd.use_position_vertical   = 1;
+  position_cmd.use_position_horizontal = 1;
+  position_cmd.use_velocity_vertical   = 1;
+  position_cmd.use_velocity_horizontal = 1;
+  position_cmd.use_heading             = 1;
+  position_cmd.use_heading_rate        = 1;
+
+  position_cmd.use_acceleration        = 0;
+  position_cmd.use_jerk                = 0;
+
+  // set the header
+  position_cmd.header.stamp    = uav_state->header.stamp;
+  position_cmd.header.frame_id = uav_state->header.frame_id;
+
+  // return a position command
+  return mrs_msgs::PositionCommand::ConstPtr(new mrs_msgs::PositionCommand(position_cmd));
+}
+//}
+
+/*getStatus()//{*/
+const mrs_msgs::TrackerStatus DergbryanTracker::getStatus() {
+  mrs_msgs::TrackerStatus tracker_status;
+  tracker_status.active = is_active_;
+  tracker_status.tracking_trajectory = false;
+  return tracker_status;
+}
+//}
+
+/*enableCallbacks()//{*/
+const std_srvs::SetBoolResponse::ConstPtr DergbryanTracker::enableCallbacks(const std_srvs::SetBoolRequest::ConstPtr &cmd) {
+
+  std_srvs::SetBoolResponse res;
+
+  res.message = "Callbacks not implemented";
+  res.success = true;
+
+  return std_srvs::SetBoolResponse::ConstPtr(new std_srvs::SetBoolResponse(res));
+}
+//}
+
+/*switchOdometrySource()//{*/
+const std_srvs::TriggerResponse::ConstPtr DergbryanTracker::switchOdometrySource(const mrs_msgs::UavState::ConstPtr &new_uav_state) {
+
+  std_srvs::TriggerResponse res;
+
+  res.message = "Switching not implemented";
+  res.success = true;
+
+  return std_srvs::TriggerResponse::ConstPtr(new std_srvs::TriggerResponse(res));
+}
+//}
+
+/*hover()//{*/
+const std_srvs::TriggerResponse::ConstPtr DergbryanTracker::hover([[maybe_unused]] const std_srvs::TriggerRequest::ConstPtr &cmd) {
+  std_srvs::TriggerResponse res;
+  res.message = "hover initiated";
+  res.success = true;
+
+  hover_ = true;
+
+  return std_srvs::TriggerResponse::ConstPtr(new std_srvs::TriggerResponse(res));
+}
+//}
+
+/*startTrajectoryTracking()//{*/
+const std_srvs::TriggerResponse::ConstPtr DergbryanTracker::startTrajectoryTracking([[maybe_unused]] const std_srvs::TriggerRequest::ConstPtr &cmd) {
+  return std_srvs::TriggerResponse::Ptr();
+}
+//}
+
+/*stopTrajectoryTracking()//{*/
+const std_srvs::TriggerResponse::ConstPtr DergbryanTracker::stopTrajectoryTracking([[maybe_unused]] const std_srvs::TriggerRequest::ConstPtr &cmd) {
+  return std_srvs::TriggerResponse::Ptr();
+}
+//}
+
+/*resumeTrajectoryTracking()//{*/
+const std_srvs::TriggerResponse::ConstPtr DergbryanTracker::resumeTrajectoryTracking([[maybe_unused]] const std_srvs::TriggerRequest::ConstPtr &cmd) {
+  return std_srvs::TriggerResponse::Ptr();
+}
+//}
+
+/*gotoTrajectoryStart()//{*/
+const std_srvs::TriggerResponse::ConstPtr DergbryanTracker::gotoTrajectoryStart([[maybe_unused]] const std_srvs::TriggerRequest::ConstPtr &cmd) {
+  return std_srvs::TriggerResponse::Ptr();
+}
+//}
+
+/*setConstraints()//{*/
+const mrs_msgs::DynamicsConstraintsSrvResponse::ConstPtr DergbryanTracker::setConstraints(const mrs_msgs::DynamicsConstraintsSrvRequest::ConstPtr &cmd) {
+
+  mrs_msgs::DynamicsConstraintsSrvResponse res;
+
+  res.success = true;
+  res.message = "No constraints to update";
+
+  return mrs_msgs::DynamicsConstraintsSrvResponse::ConstPtr(new mrs_msgs::DynamicsConstraintsSrvResponse(res));
+}
+//}
+
+/*setReference()//{*/
+const mrs_msgs::ReferenceSrvResponse::ConstPtr DergbryanTracker::setReference(const mrs_msgs::ReferenceSrvRequest::ConstPtr &cmd) {
+
+  mrs_msgs::ReferenceSrvResponse res;
+
+  {
+    std::scoped_lock lock(mutex_goal_);
+
+  goal_x_=cmd->reference.position.x;
+  goal_y_=cmd->reference.position.y;
+  goal_z_=cmd->reference.position.z;
+  goal_heading_=cmd->reference.heading;
+
+  }
+
+  hover_ = false;
+
+  res.success = true;
+  res.message = "reference set";
+
+  return mrs_msgs::ReferenceSrvResponse::ConstPtr(new mrs_msgs::ReferenceSrvResponse(res));
+}
+//}
+
+/*setTrajectoryReference()//{*/
+const mrs_msgs::TrajectoryReferenceSrvResponse::ConstPtr DergbryanTracker::setTrajectoryReference([
+    [maybe_unused]] const mrs_msgs::TrajectoryReferenceSrvRequest::ConstPtr &cmd) {
+  return mrs_msgs::TrajectoryReferenceSrvResponse::Ptr();
+}
+//}
+
+}  // namespace dergbryan_tracker
+}  // namespace mrs_uav_trackers
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(mrs_uav_trackers::dergbryan_tracker::DergbryanTracker, mrs_uav_managers::Tracker)
