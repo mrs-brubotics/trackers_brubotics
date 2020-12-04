80f33028bd4d6286e07fc009e26cbe44e96edb6b
Bryan Convens <bryan.convens@gmail.com>
2020-12-04 15:25:04 +0100

    copy + change Se3Controller code up to velocity error calculation

--- src/dergbryan_tracker/dergbryan_tracker.cpp
+++ src/dergbryan_tracker/dergbryan_tracker.cpp
@@ -6,6 +6,10 @@
 #include <mrs_lib/profiler.h>
 #include <mrs_lib/mutex.h>
 
+/*begin includes added by bryan:*/
+#include <mrs_lib/attitude_converter.h>
+/*end includes added by bryan:*/
+
 //}
 
 namespace mrs_uav_trackers
@@ -149,7 +153,7 @@ const mrs_msgs::PositionCommand::ConstPtr DergbryanTracker::update(const mrs_msg
   position_cmd.header.stamp    = uav_state->header.stamp;
   position_cmd.header.frame_id = uav_state->header.frame_id;
   if (starting_bool) {
-
+    // stay in place
     goal_x_= uav_state->pose.position.x;
     goal_y_= uav_state->pose.position.y;
     goal_z_= uav_state->pose.position.z;
@@ -183,27 +187,179 @@ const mrs_msgs::PositionCommand::ConstPtr DergbryanTracker::update(const mrs_msg
  
   return mrs_msgs::PositionCommand::ConstPtr(new mrs_msgs::PositionCommand(position_cmd));
 }
+/* begin copy of se3controller*/
 
- // set the desired states from the input of the goto function
+// | -------------------- calculate the dt -------------------- |
 
+  double dt;
+  /* TODO: this if condition is commented since we will only allow this tracker to be activated if before the se3 was activated. 
+  so will need to see how to use const mrs_msgs::ControllerStatus Se3Controller::getStatus() { of the controller in here to check if it is active*/
+
+  // if (first_iteration_) {
+
+  //   last_update_time_ = uav_state->header.stamp;
+
+  //   first_iteration_ = false;
+
+  //   ROS_INFO("[Se3Controller]: first iteration");
+
+  //   return mrs_msgs::AttitudeCommand::ConstPtr(new mrs_msgs::AttitudeCommand(activation_attitude_cmd_));
+
+  // } else {
+
+  /* NOTE: assume a fixed sampling time used in the controller for this DergTracker*/
+  /* TODO: load dt (se3 control sample time) as inverse of control sampling frequency defined somewhere in mrs tracker.yaml files (e.g. see mpc, line trackers) and set to 100Hz for controller (*/
+  dt = 0.01; 
+  // dt                = (uav_state->header.stamp - last_update_time_).toSec();
+  // last_update_time_ = uav_state->header.stamp;
+  // }
+
+  /* NOTE: commented this section bacause we assume the controller has a fixed sample time of dt*/
+  // if (fabs(dt) <= 0.001) {
+
+  //   ROS_DEBUG("[Se3Controller]: the last odometry message came too close (%.2f s)!", dt);
+
+  //   if (last_attitude_cmd_ != mrs_msgs::AttitudeCommand::Ptr()) {
+
+  //     return last_attitude_cmd_;
+
+  //   } else {
+
+  //     return mrs_msgs::AttitudeCommand::ConstPtr(new mrs_msgs::AttitudeCommand(activation_attitude_cmd_));
+  //   }
+  // }
+
+  // | ----------------- get the current heading ---------------- |
+
+  double uav_heading = 0;
+
+  try {
+    uav_heading = mrs_lib::AttitudeConverter(uav_state->pose.orientation).getHeading();
+  }
+  catch (...) {
+    ROS_ERROR_THROTTLE(1.0, "[DergbryanTracker]: could not calculate the UAV heading");
+  }
+
+  
+
+  // --------------------------------------------------------------
+  // |          load the control reference and estimates          |
+  // --------------------------------------------------------------
+
+  // Rp - position reference in global frame
+  // Rv - velocity reference in global frame
+  // Ra - velocity reference in global frame
+  // Rw - angular velocity reference
+
+  Eigen::Vector3d Rp = Eigen::Vector3d::Zero(3);
+  Eigen::Vector3d Rv = Eigen::Vector3d::Zero(3);
+  Eigen::Vector3d Ra = Eigen::Vector3d::Zero(3);
+  Eigen::Vector3d Rw = Eigen::Vector3d::Zero(3);
+  
+  position_cmd.use_position_vertical   = 1;
+  position_cmd.use_position_horizontal = 1;
+  // QUESTION: Why can't I print this? No value is printed, empty.
+  // ROS_INFO_STREAM("position_cmd.use_position_horizontal = \n" << position_cmd.use_position_horizontal);
+  // std::cout << "with std: "<< position_cmd.use_position_horizontal << std::endl;
+  position_cmd.use_velocity_vertical   = 1;
+  position_cmd.use_velocity_horizontal = 1;
+  position_cmd.use_acceleration        = 0;
+  position_cmd.use_jerk                = 0;
+  position_cmd.use_heading             = 1;
+  position_cmd.use_heading_rate        = 1;
+
+  /*TODO,ADDED: SET THE APPLIED REFERNECE POSE (POSITION & HEADING) TEMPORARILY TO THE DESIRED GOAL*/
   position_cmd.position.x     = goal_x_;
   position_cmd.position.y     = goal_y_;
   position_cmd.position.z     = goal_z_;
   position_cmd.heading        = goal_heading_;
 
-  position_cmd.use_position_vertical   = 1;
-  position_cmd.use_position_horizontal = 1;
-  position_cmd.use_velocity_vertical   = 1;
-  position_cmd.use_velocity_horizontal = 1;
-  position_cmd.use_heading             = 1;
-  position_cmd.use_heading_rate        = 1;
+  /* NOTE: replaced control_reference -> (input of controller) by position_cmd.use_ defined above in the update function*/
+  if (position_cmd.use_position_vertical || position_cmd.use_position_horizontal) {
+    // ROS_INFO_STREAM("position_cmd.use_position_horizontal = \n" << position_cmd.use_position_horizontal);
+    // std::cout << "with std: "<< position_cmd.use_position_horizontal << std::endl;
+    if (position_cmd.use_position_horizontal) {
+      
+      Rp[0] = position_cmd.position.x;
+      Rp[1] = position_cmd.position.y;
+    } else {
+      Rv[0] = 0;
+      Rv[1] = 0;
+    }
 
-  position_cmd.use_acceleration        = 0;
-  position_cmd.use_jerk                = 0;
+    if (position_cmd.use_position_vertical) {
+      Rp[2] = position_cmd.position.z;
+    } else {
+      Rv[2] = 0;
+    }
+  }
 
-  // set the header
-  position_cmd.header.stamp    = uav_state->header.stamp;
-  position_cmd.header.frame_id = uav_state->header.frame_id;
+  if (position_cmd.use_velocity_horizontal) {
+    Rv[0] = position_cmd.velocity.x;
+    Rv[1] = position_cmd.velocity.y;
+  } else {
+    Rv[0] = 0;
+    Rv[1] = 0;
+  }
+
+  if (position_cmd.use_velocity_vertical) {
+    Rv[2] = position_cmd.velocity.z;
+  } else {
+    Rv[2] = 0;
+  }
+
+  if (position_cmd.use_acceleration) {
+    Ra << position_cmd.acceleration.x, position_cmd.acceleration.y, position_cmd.acceleration.z;
+  } else {
+    Ra << 0, 0, 0;
+  }
+  /*VALIDATE: test streaming the references Rp, Rv, Ra when the drone is moving.*/
+  // ROS_INFO_STREAM("Rp = \n" << Rp);
+  // ROS_INFO_STREAM("Rv = \n" << Rv);
+  // ROS_INFO_STREAM("Ra = \n" << Ra);
+ 
+  /*QUESTION: where to print Rw? When it is set*/
+  // ROS_INFO_STREAM("Rw = \n" << Rw);
+
+  // Op - position in global frame
+  // Ov - velocity in global frame
+  Eigen::Vector3d Op(uav_state->pose.position.x, uav_state->pose.position.y, uav_state->pose.position.z);
+  Eigen::Vector3d Ov(uav_state->velocity.linear.x, uav_state->velocity.linear.y, uav_state->velocity.linear.z);
+
+  // R - current uav attitude
+  Eigen::Matrix3d R = mrs_lib::AttitudeConverter(uav_state->pose.orientation);
+
+  // Ow - UAV angular rate
+  Eigen::Vector3d Ow(uav_state->velocity.angular.x, uav_state->velocity.angular.y, uav_state->velocity.angular.z);
+
+  // // | -------------- calculate the control errors -------------- |
+
+  // position control error
+  Eigen::Vector3d Ep = Eigen::Vector3d::Zero(3);
+
+  if (position_cmd.use_position_horizontal || position_cmd.use_position_vertical) {
+    Ep = Op - Rp;
+  }
+
+  // velocity control error
+  Eigen::Vector3d Ev = Eigen::Vector3d::Zero(3);
+
+  if (position_cmd.use_velocity_horizontal || position_cmd.use_velocity_vertical ||
+      position_cmd.use_position_vertical) {  // even when use_position_vertical to provide dampening
+    Ev = Ov - Rv;
+  }
+  /*VALIDATE: test streaming the references Op, Ov, R, Ow, Ep, Ev when the drone is moving.*/
+  // ROS_INFO_STREAM("Op = \n" << Op);
+  // ROS_INFO_STREAM("Ov = \n" << Ov);
+  // ROS_INFO_STREAM("R  = \n" << R);
+  // ROS_INFO_STREAM("Ow = \n" << Ow);
+  // ROS_INFO_STREAM("Ep = \n" << Ep);
+  // ROS_INFO_STREAM("Ev = \n" << Ev);
+/* end copy of se3controller*/
+ // set the desired states from the input of the goto function
+
+
+  
 
   // return a position command
   return mrs_msgs::PositionCommand::ConstPtr(new mrs_msgs::PositionCommand(position_cmd));
