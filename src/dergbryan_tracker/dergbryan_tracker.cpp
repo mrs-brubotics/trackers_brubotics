b4355c6806bf600489a16d08c84dd918d01d0aab
JonathanVogt <jonathan.vogt@etu.unistra.fr>
2021-08-10 15:13:12 +0200

    Added Sa_max and Sa_perp_max publishers and added point_link_star publisher in each strategy

--- src/dergbryan_tracker/dergbryan_tracker.cpp
+++ src/dergbryan_tracker/dergbryan_tracker.cpp
@@ -289,7 +289,11 @@ private:
   // added by Titouan and Jonathan
   ros::Publisher derg_strategy_id_publisher_;
   ros::Publisher point_link_star_publisher_;
+  ros::Publisher sa_max_publisher_;
+  ros::Publisher sa_perp_max_publisher_;
   std_msgs::Int32 DERG_strategy_id;
+  std_msgs::Int32 Sa_max;
+  std_msgs::Int32 Sa_perp_max;
   geometry_msgs::Pose point_link_star_;
   
   void callbackOtherUavAppliedRef(const mrs_msgs::FutureTrajectoryConstPtr& msg);
@@ -647,6 +651,8 @@ void DergbryanTracker::initialize(const ros::NodeHandle &parent_nh, [[maybe_unus
   // added by Titouan and Jonathan
   derg_strategy_id_publisher_ = nh2_.advertise<std_msgs::Int32>("derg_strategy_id", 1);
   point_link_star_publisher_ = nh2_.advertise<geometry_msgs::Pose>("point_link_star", 1);
+  sa_max_publisher_ = nh2_.advertise<std_msgs::Int32>("sa_max", 1);
+  sa_perp_max_publisher_ = nh2_.advertise<std_msgs::Int32>("sa_perp_max", 1);
 
 
 
@@ -2901,6 +2907,9 @@ void DergbryanTracker::DERG_computation(){
       
     }
 
+  // added by Titouan and Jonathan
+  tf::pointEigenToMsg(point_link_star, point_link_star_.position);  // conversion from Eigen::Vector3d to geometry_msgs::Point
+
   }
     if (_DERG_strategy_id_ == 3) {
     /* D-ERG strategy 3: a tube of variable length _Sa_long_ and fixed width _Sa_perp_max_. 
@@ -3108,6 +3117,10 @@ void DergbryanTracker::DERG_computation(){
       //it2++;
       //it3++;
     }
+
+  // added by Titouan and Jonathan
+  tf::pointEigenToMsg(point_link_star, point_link_star_.position);  // conversion from Eigen::Vector3d to geometry_msgs::Point
+  
   }
 
     if (_DERG_strategy_id_ == 4) {
@@ -3346,6 +3359,8 @@ void DergbryanTracker::DERG_computation(){
       //it2++;
       //it3++;
     }
+  // added by Titouan and Jonathan
+  tf::pointEigenToMsg(point_link_star, point_link_star_.position);  // conversion from Eigen::Vector3d to geometry_msgs::Point
   }
 
   if (_DERG_strategy_id_ == 5) {
@@ -3544,6 +3559,9 @@ void DergbryanTracker::DERG_computation(){
       }
       it1++;
     } 
+
+  // added by Titouan and Jonathan
+  tf::pointEigenToMsg(point_link_star, point_link_star_.position);  // conversion from Eigen::Vector3d to geometry_msgs::Point
   }
 
 
@@ -3653,6 +3671,10 @@ void DergbryanTracker::DERG_computation(){
   DERG_strategy_id.data = _DERG_strategy_id_;
   derg_strategy_id_publisher_.publish(DERG_strategy_id);
   point_link_star_publisher_.publish(point_link_star_);
+  Sa_max.data= _Sa_max_;
+  sa_max_publisher_.publish(Sa_max);
+  Sa_perp_max.data= _Sa_perp_max_;
+  sa_perp_max_publisher_.publish(Sa_perp_max);
 
 }
 
